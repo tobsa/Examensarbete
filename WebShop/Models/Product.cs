736cba82a806db433070f06b69cfe10dac5cee2e
Tobias Savinainen <tobias@savinainen.se>
2015-04-08 15:01:55 +0200

    Added thumbnail url

--- WebShop/Models/Product.cs
+++ WebShop/Models/Product.cs
@@ -12,6 +12,7 @@ namespace WebShop.Models
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
         public int CategoryId { get; set; }
     }
 }
